456000faae514de2d10f82f5abbe436ae00a1484
Ch1charito <r.buschmann.work@gmail.com>
2025-06-21 16:13:26 +0200

    added why me section

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
+import { WhyMeComponent } from "./why-me/why-me.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeaderComponent, AboveTheFoldComponent],
+  imports: [HeaderComponent, AboveTheFoldComponent, WhyMeComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
