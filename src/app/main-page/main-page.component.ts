8a30fdb6c1e7895019c57a7023d4744351719af5
Ch1charito <r.buschmann.work@gmail.com>
2025-06-25 17:24:06 +0200

    added contact component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -6,10 +6,11 @@ import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { MyProjectsComponent } from "./my-projects/my-projects.component";
 import { TeamplayerComponent } from "./teamplayer/teamplayer.component";
 import { FooterComponent } from "./footer/footer.component";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeaderComponent, AboveTheFoldComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, TeamplayerComponent, FooterComponent],
+  imports: [HeaderComponent, AboveTheFoldComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, TeamplayerComponent, FooterComponent, ContactFormComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
